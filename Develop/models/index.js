549df5ca804f49a26f0529438455f3f18ecd924a
nikkivno <nikkivigneault@gmail.com>
2023-07-28 13:22:09 -0300

    missing comma added in index.js in models folder

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -19,7 +19,7 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 
-Product.belongsToMany(Tag {
+Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: product_id
 });
